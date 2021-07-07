f5f963448e81d9089211f37ab0c893fec99afb7c
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-07-07 10:29:44 +0900

    자바 프로젝트

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static final int SCREEN_WIDTH;
+    static
+    {
+        SCREEN_WIDTH=1920;
+    }
+    public static final int SCREEN_HEIGHT;
+    static {
+        SCREEN_HEIGHT = 1080;
+    }
+    public static void main(String[] args)
+    {
+        new Avoid_Game();
+    }
+}
