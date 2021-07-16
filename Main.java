88b915fc64acfa786911b9683b381635adc90f97
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-07-16 22:33:57 +0900

    java 프로젝트

--- Main.java
+++ Main.java
@@ -1,16 +1,11 @@
+package com.company;
 public class Main {
-    /*화면 출력*/
-    public static final int SCREEN_WIDTH;
-    static
-    {
-        SCREEN_WIDTH=1920;
-    }
-    public static final int SCREEN_HEIGHT;
-    static {
-        SCREEN_HEIGHT = 1080;
-    }
-    public static void main(String[] args)
-    {
-        new Avoid_Game();
+    /*SCREEN_WIDTH(화면의 넓이)*/
+    public static final int SCREEN_WIDTH=1280;
+    /*SCREEN_HEIGHT(전체 높이)*/
+    public static final int SCREEN_HEIGHT=720;
+
+    public static void main(String[] args) {
+        new DynamicBeat();
     }
 }
