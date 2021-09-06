5cdddbffc85dd68fed9b8e02b0eb71b23d2ce3ba
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2021-09-06 15:06:59 +0900

    Java(프로그래밍(학교 수업)

---
+++ Ex_09.java
@@ -0,0 +1,19 @@
+package Chap02;
+
+import java.util.Scanner;
+
+public class Ex_09 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		while(true)
+		{
+			String str=sc.next();
+			if(str.equals("exit")){
+				break;
+			}
+		}
+
+	}
+
+}
