2f4b99a8a8a32398cc71b0d62782b39a3c961d00
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2021-09-14 11:28:53 +0900

    Java(학교수업)

---
+++ UtilDemo.java
@@ -0,0 +1,15 @@
+package Chap04;
+
+class utill{
+	static int fourTiems(int i) {
+		return i*4;
+	}
+}
+
+public class UtilDemo {
+
+	public static void main(String[] args) {
+		System.out.println(utill.fourTiems(5));
+	}
+
+}
