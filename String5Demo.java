3d6753b6d180d3497e999502c557f344bc7130e4
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2021-10-01 11:09:18 +0900

    Java(학교수업-프로그래밍(배열)

---
+++ String5Demo.java
@@ -0,0 +1,14 @@
+package Chap05_02;
+
+public class String5Demo {
+	public static void main(String[] args) {
+		String version=String.format("%s %d","JDK",14);
+		System.out.println(version);
+		
+		String fruits=String.join(", ","apple","banna","cherry","durian");
+		System.out.println(fruits);
+		
+		String pi=String.valueOf(3.14);
+		System.out.println(pi);
+	}
+}
