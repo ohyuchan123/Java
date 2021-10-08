c36cfa6a03d9e12ff794d72805db7d2922562f09
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2021-10-08 10:18:45 +0900

    Java(프로그래밍-학교수업(상속)

---
+++ Circle11Test.java
@@ -0,0 +1,10 @@
+package Test;
+
+import Chap06.Circle11;
+
+public class Circle11Test {
+	public static void main(String[] args) {
+		Circle11 c1=new Circle11(5);
+		c1.findArea();
+	}
+}
---
+++ Circle11test.java
@@ -0,0 +1,15 @@
+package Chap06;
+
+public class Circle11test {
+	public static void main(String[] args) {
+		Cylinder cy=new Cylinder(10,5);
+		Ball1 ba=new Ball1(10,5);
+		Cone co=new Cone(10,5);
+		
+		cy.findArea();
+		ba.findArea();
+		co.findArea();
+
+	}
+}
+
